aa0c7e00df7a2f3b466b97304ce25733627b8510
Saul Rodrigues Alcântara <sauldesigner.ce@gmail.com>
2022-09-25 15:35:54 -0300

    Sprites, Sounds, Menu, Text Fonts

---
+++ Jokenpo - treino - gjplus/Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public float transitionDuration;
+
+    public bool pausedGame;
+    public bool beginGame;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    private void Start() {
+
+    }
+
+    public void LoadScene(int newScene) {
+        StartCoroutine(Load(newScene));
+    }
+
+    public IEnumerator Load(int newScene) {
+        yield return new WaitForSeconds(transitionDuration);
+        SceneManager.LoadSceneAsync(newScene);
+    }
+    public void ReloadScene() {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void BeginGame() {
+        beginGame = true;
+    }
+
+    public void EndGame() {
+        beginGame = false;
+    }
+
+    public void PauseGame() {
+        pausedGame = !pausedGame;
+        beginGame = !pausedGame;
+
+        if (AudioManager.instance != null)
+            AudioManager.instance.MusicPauseGame(pausedGame);
+        else Debug.Log("Audio Manager is Missing");
+
+        if (pausedGame) Time.timeScale = 0;
+        else Time.timeScale = 1;
+    }
+
+    public void ShowCanvasGroup(CanvasGroup CG) {
+        if (CG != null) {
+            StartCoroutine(ShowCanvasGroupAlpha(CG));
+        } else {
+            Debug.Log("Panel with Canvas Groups is Missing");
+        }
+    }
+
+    IEnumerator ShowCanvasGroupAlpha(CanvasGroup CG) {
+        CG.blocksRaycasts = true;
+        float currentTime = 0f;
+        while (currentTime < transitionDuration) {
+            CG.alpha = Mathf.Lerp(0, 1, currentTime / transitionDuration);
+            currentTime += Time.fixedDeltaTime;
+            yield return null;
+        }
+        CG.alpha = 1;
+        yield return null;               
+    }
+
+    public void HideCanvasGroup(CanvasGroup CG) {
+        if (CG != null) {
+            StartCoroutine(HideCanvasGroupAplha(CG));
+        } else {
+            Debug.Log("Panel with Canvas Groups is Missing");
+        }
+    }
+
+    IEnumerator HideCanvasGroupAplha(CanvasGroup CG) {
+        CG.blocksRaycasts = false;
+        float currentTime = 0f;
+        while (currentTime < transitionDuration) {
+            CG.alpha = Mathf.Lerp(1, 0, currentTime / transitionDuration);
+            currentTime += Time.fixedDeltaTime;
+            yield return null;
+        }
+        CG.alpha = 0;
+        yield return null;        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
